7c7dcfcb39660e6a5a58df4c0d3c2f8103f7ff56
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-12 17:36:59 -0500

    style ManageExpense.jsx, Button.jsx

--- components/ExpensesOutput/ExpenseItem.jsx
+++ components/ExpensesOutput/ExpenseItem.jsx
@@ -3,10 +3,12 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 import { useNavigation } from "@react-navigation/native";
 
-const ExpenseItem = ({ description, amount, date }) => {
+const ExpenseItem = ({ description, amount, date, id }) => {
 	const navigation = useNavigation();
 	function expensePressHandler() {
-		navigation.navigate("ManageExpense");
+		navigation.navigate("ManageExpense", {
+			expenseId: id,
+		});
 	}
 	return (
 		<Pressable
--- screens/ManageExpense.jsx
+++ screens/ManageExpense.jsx
@@ -1,7 +1,70 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import Button from "../components/UI/Button";
+import { GlobalStyles } from "../constants/styles";
 
-const ManageExpense = () => {
-	return <Text>ManageExpense</Text>;
+const ManageExpense = ({ route, navigation }) => {
+	const editedExpenseId = route?.params?.expenseId;
+	const isEditing = !!editedExpenseId; //convert a value into a boolean. falsey value to false, truthy value to true
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: isEditing ? "Edit Expense" : "Add Expense",
+		});
+	}, [navigation, isEditing]);
+
+	function deleteExpenseHandler() {}
+
+	function cancelHandler() {}
+
+	function confirmHandler() {}
+	return (
+		<View style={styles.container}>
+			<View style={styles.buttons}>
+				<Button mode="flat" onPress={cancelHandler} style={styles.button}>
+					Cancel
+				</Button>
+				<Button onPress={confirmHandler} style={styles.button}>
+					{isEditing ? "Update" : "Add"}
+				</Button>
+			</View>
+
+			{isEditing && (
+				<View style={styles.deleteContainer}>
+					<IconButton
+						icon="trash"
+						color={GlobalStyles.colors.error500}
+						size={36}
+						onPress={deleteExpenseHandler}
+					/>
+				</View>
+			)}
+		</View>
+	);
 };
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 24,
+		backgroundColor: GlobalStyles.colors.primary800,
+	},
+	buttons: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	button: {
+		minWidth: 120,
+		marginHorizontal: 8,
+	},
+	deleteContainer: {
+		marginTop: 16,
+		paddingTop: 8,
+		borderTopWidth: 2,
+		borderTopColor: GlobalStyles.colors.primary200,
+		alignItems: "center",
+	},
+});
 export default ManageExpense;
