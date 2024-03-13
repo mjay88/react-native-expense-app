5c0229e1a546a927465a0fe0741372967a2439ab
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-12 23:36:54 -0500

    wire up Input components and configure them with custom configuration

--- components/ManageExpense/ExpenseForm.jsx
+++ components/ManageExpense/ExpenseForm.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { View } from "react-native";
+import Input from "./Input";
 
 const ExpenseForm = () => {
+	function amountChangedHandler() {}
 	return (
 		<View>
-			<TextInput></TextInput>
+			<Input
+				label="Amount"
+				textInputConfig={{
+					keyboardType: "decimal-pad",
+					onChangeText: amountChangedHandler,
+				}}
+			/>
+			<Input
+				label="Date"
+				textInputConfig={{
+					placeholder: "YYYY-MM-DD",
+					maxLength: 10,
+					onChangeText: () => {},
+				}}
+			/>
+			<Input
+				label="Description"
+				textInputConfig={{
+					multiline: true,
+					// autoCapitalize: "none" //default is sentences
+					// autoCorrect: false, //default is true
+				}}
+			/>
 		</View>
 	);
 };
--- screens/ManageExpense.jsx
+++ screens/ManageExpense.jsx
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 const ManageExpense = ({ route, navigation }) => {
 	const expensesCtx = useContext(ExpensesContext);
@@ -43,6 +44,7 @@ const ManageExpense = ({ route, navigation }) => {
 	}
 	return (
 		<View style={styles.container}>
+			<ExpenseForm />
 			<View style={styles.buttons}>
 				<Button mode="flat" onPress={cancelHandler} style={styles.button}>
 					Cancel
