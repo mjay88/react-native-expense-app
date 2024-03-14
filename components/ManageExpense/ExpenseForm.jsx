75c3d3ed7f3ed7725e44d6290cfb534e5e7d4af1
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-14 17:13:26 -0500

    adding validation

--- components/ManageExpense/ExpenseForm.jsx
+++ components/ManageExpense/ExpenseForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 import Button from "../UI/Button";
 import { getFormattedDate } from "../../util/date";
@@ -10,30 +10,63 @@ const ExpenseForm = ({
 	submitButtonLabel,
 	defaultValues,
 }) => {
-	const [inputValues, setInputValues] = useState({
-		amount: defaultValues ? defaultValues.amount.toString() : "",
-		date: defaultValues ? getFormattedDate(defaultValues.date) : "",
-		description: defaultValues ? defaultValues.description : "",
+	const [inputs, setInputs] = useState({
+		amount: {
+			value: defaultValues ? defaultValues.amount.toString() : "",
+			isValid: true,
+		},
+		date: {
+			value: defaultValues ? getFormattedDate(defaultValues.date) : "",
+			isValid: true,
+		},
+		description: {
+			value: defaultValues ? defaultValues.description : "",
+			isValid: true,
+		},
 	});
 
 	function inputChangedHandler(inputIdentifier, enteredValue) {
-		setInputValues((curInputValues) => {
+		setInputs((curInputs) => {
 			return {
-				...curInputValues,
+				...curInputs,
 				//dynamic property name
-				[inputIdentifier]: enteredValue,
+				[inputIdentifier]: { value: enteredValue, isValid: true },
 			};
 		});
 	}
 
 	function submitHandler() {
 		const expenseData = {
-			amount: +inputValues.amount,
-			date: new Date(inputValues.date),
-			description: inputValues.description,
+			amount: +inputs.amount.value,
+			date: new Date(inputs.date.value),
+			description: inputs.description.value,
 		};
+
+		const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+		const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+		const descriptionIsValid = expenseData.description.trim().length > 0;
+
+		if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+			// Alert.alert("Invalid input", "Please check your input values");
+			setInputs((currInputs) => {
+				return {
+					amount: { value: currInputs.amount.value, isValid: amountIsValid },
+					date: { value: currInputs.date.value, isValid: dateIsValid },
+					description: {
+						value: currInputs.description.value,
+						isValid: descriptionIsValid,
+					},
+				};
+			});
+			return;
+		}
 		onSubmit(expenseData);
 	}
+
+	const formIsInvalid =
+		!inputs.amount.isValid ||
+		!inputs.date.isValid ||
+		!inputs.description.isValid;
 
 	return (
 		<View style={styles.form}>
@@ -46,7 +79,7 @@ const ExpenseForm = ({
 						keyboardType: "decimal-pad",
 						//the second arguement passed to bind will be the first argument passed to inputChangedHandler
 						onChangeText: inputChangedHandler.bind(this, "amount"),
-						value: inputValues.amount,
+						value: inputs.amount.value,
 					}}
 				/>
 				<Input
@@ -56,7 +89,7 @@ const ExpenseForm = ({
 						placeholder: "YYYY-MM-DD",
 						maxLength: 10,
 						onChangeText: inputChangedHandler.bind(this, "date"),
-						value: inputValues.date,
+						value: inputs.date.value,
 					}}
 				/>
 			</View>
@@ -67,9 +100,12 @@ const ExpenseForm = ({
 					// autoCapitalize: "none" //default is sentences
 					// autoCorrect: false, //default is true
 					onChangeText: inputChangedHandler.bind(this, "description"),
-					value: inputValues.description,
+					value: inputs.description.value,
 				}}
 			/>
+			{formIsInvalid && (
+				<Text>Invalid input values- please check your entered data!</Text>
+			)}
 			<View style={styles.buttons}>
 				<Button mode="flat" onPress={onCancel} style={styles.button}>
 					Cancel
